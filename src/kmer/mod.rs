7f6d0dcb86efa734ba4e805918f31ff3cff72af5
Pierre Marijon <pmarijon@mpi-inf.mpg.de>
2019-12-18 11:00:35 +0100

    version 0.1-beta

---
+++ src/kmer/mod.rs
@@ -0,0 +1,187 @@
+/*
+Copyright (c) 2019 Pierre Marijon <pmarijon@mpi-inf.mpg.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+pub fn seq2bit(subseq: &[u8]) -> u64 {
+    let mut kmer: u64 = 0;
+
+    for n in subseq {
+        kmer <<= 2;
+        kmer |= nuc2bit(*n);
+    }
+
+    kmer
+}
+
+#[inline(always)]
+pub fn nuc2bit(nuc: u8) -> u64 {
+    (nuc as u64 >> 1) & 0b11
+}
+
+pub fn kmer2seq(mut kmer: u64, k: u8) -> String {
+    let mut result = vec![0; k as usize];
+
+    for i in (0..k).rev() {
+        let val = kmer & 0b11;
+
+        result[i as usize] = bit2nuc(val);
+
+        kmer >>= 2;
+    }
+
+    String::from_utf8(result).unwrap()
+}
+
+#[inline(always)]
+pub fn bit2nuc(bit: u64) -> u8 {
+    match bit {
+        0 => b'A',
+        1 => b'C',
+        2 => b'T',
+        3 => b'G',
+        _ => b'G',
+    }
+}
+
+#[inline(always)]
+pub fn cannonical(kmer: u64, k: u8) -> u64 {
+    if parity_even(kmer) {
+        kmer
+    } else {
+        revcomp(kmer, k)
+    }
+}
+
+#[inline(always)]
+fn parity_even(kmer: u64) -> bool {
+    kmer.count_ones() % 2 == 0
+}
+
+pub fn revcomp(kmer: u64, k: u8) -> u64 {
+    rev(comp(kmer), k)
+}
+
+#[inline(always)]
+pub fn comp(kmer: u64) -> u64 {
+    kmer ^ 0b1010_1010_1010_1010_1010_1010_1010_1010_1010_1010_1010_1010_1010_1010_1010_1010
+}
+
+#[inline(always)]
+pub fn get_first_bit(kmer: u64) -> bool {
+    kmer & 1 != 0
+}
+
+#[inline(always)]
+pub fn remove_first_bit(kmer: u64) -> u64 {
+    kmer >> 1
+}
+
+pub fn hash(subseq: &[u8], k: u8) -> u64 {
+    remove_first_bit(cannonical(seq2bit(subseq), k))
+}
+
+mod lookup_table;
+
+pub fn rev(mut kmer: u64, k: u8) -> u64 {
+    let nb_bit = k * 2;
+    let mut reverse: u64 = 0;
+
+    let nb_block = 1 + nb_bit / 8; //odd kmer never fit in 8 block
+
+    for i in 0..nb_block {
+        reverse ^= (lookup_table::REVERSE_2_LOOKUP[(kmer & 255) as u8 as usize] as u64)
+            << ((nb_block - i - 1) * 8);
+
+        kmer >>= 8;
+    }
+
+    reverse >> (nb_block * 8 - nb_bit)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn seq2bit_() {
+        // TAGGC -> 1000111101
+        assert_eq!(seq2bit(b"TAGGC"), 0b1000111101);
+
+        // GCCTA -> 110101100
+        assert_eq!(seq2bit(b"GCCTA"), 0b1101011000);
+    }
+
+    #[test]
+    fn bit2seq_() {
+        // 1000111101 -> TAGGC
+        assert_eq!(kmer2seq(0b1000111101, 5), "TAGGC");
+
+        // 110101100 -> GCCTA
+        assert_eq!(kmer2seq(0b1101011000, 5), "GCCTA");
+    }
+
+    #[test]
+    fn cannonical_() {
+        // TAGGC -> 1000111101 cannonical TAGGC -> 1000111101
+        assert_eq!(cannonical(0b1000111101, 5), 0b1000111101);
+
+        // GCCTA -> 1101011000 cannonical TAGGC -> 1000111101
+        assert_eq!(cannonical(0b1101011000, 5), 0b1000111101);
+    }
+
+    #[test]
+    fn parity_even_() {
+        assert_eq!(parity_even(0b1111), true);
+        assert_eq!(parity_even(0b1110), false);
+    }
+
+    #[test]
+    fn revcomp_() {
+        // TAGGC -> 1000111101 revcomp GCCTA -> 1101011000
+        assert_eq!(0b1000111101, revcomp(0b1101011000, 5))
+    }
+
+    #[test]
+    fn comp_() {
+        // TAGGC -> 1000111101 comp 0001001011
+        assert_eq!(
+            comp(0b1000111101),
+            0b1010101010101010101010101010101010101010101010101010100010010111
+        );
+    }
+
+    #[test]
+    fn rev_() {
+        // TAGGC -> 1000111101 rev CGGAT -> 0111110010
+        let var = 0b1000111101;
+
+        assert_eq!(498, rev(var, 5));
+    }
+
+    #[test]
+    fn hash_() {
+        // TAGGC -> 100011110
+        assert_eq!(hash(b"TAGGC", 5), 0b100011110);
+
+        // GCCTA -> 110101100
+        assert_eq!(hash(b"GCCTA", 5), 0b100011110);
+    }
+}
---
+++ src/tokenizer.rs
@@ -0,0 +1,94 @@
+/*
+Copyright (c) 2019 Pierre Marijon <pmarijon@mpi-inf.mpg.de>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
+use crate::kmer;
+
+pub struct Tokenizer<'a> {
+    kmer_mask: u64,
+    seq: &'a [u8],
+    pos: usize,
+    kmer: u64,
+}
+
+impl<'a> Tokenizer<'a> {
+    pub fn new(seq: &'a [u8], k: u8) -> Self {
+        Tokenizer {
+            kmer_mask: (1 << (k * 2)) - 1,
+            seq,
+            pos: (k - 1) as usize,
+            kmer: kmer::seq2bit(&seq[0..((k - 1) as usize)]),
+        }
+    }
+}
+
+impl<'a> Iterator for Tokenizer<'a> {
+    type Item = u64;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.pos == self.seq.len() {
+            None
+        } else {
+            self.kmer = ((self.kmer << 2) & self.kmer_mask) | kmer::nuc2bit(self.seq[self.pos]);
+
+            self.pos += 1;
+
+            Some(self.kmer)
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        for kmer in Tokenizer::new(b"ACTGACTG", 5) {
+            println!("{:010b}", kmer);
+        }
+
+        assert_eq!(
+            vec![108, 433, 710, 795],
+            Tokenizer::new(b"ACTGACTG", 5).collect::<Vec<u64>>()
+        );
+    }
+
+    #[test]
+    fn hash() {
+        assert_eq!(
+            vec![54, 457, 114, 397],
+            Tokenizer::new(b"ACTGACTG", 5)
+                .map(|x| kmer::remove_first_bit(kmer::cannonical(x, 5)))
+                .collect::<Vec<u64>>()
+        );
+    }
+
+    #[test]
+    fn cannonical() {
+        assert_eq!(
+            vec![108, 915, 228, 795],
+            Tokenizer::new(b"ACTGACTG", 5)
+                .map(|x| kmer::cannonical(x, 5))
+                .collect::<Vec<u64>>()
+        );
+    }
+}
